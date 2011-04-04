c098e500cbd1458950f0777d9dbd219efd82261b
danielestevez <daniel.estevez@abiquo.com>
2011-04-04 15:16:45 +0200

    Enterprise creation via API shouldn't check Limits for new Enterprises

--- api/src/main/java/com/abiquo/api/services/EnterpriseService.java
+++ api/src/main/java/com/abiquo/api/services/EnterpriseService.java
@@ -43,6 +43,7 @@ import com.abiquo.api.resources.DatacentersResource;
 import com.abiquo.api.util.URIResolver;
 import com.abiquo.model.rest.RESTLink;
 import com.abiquo.model.transport.error.ErrorsDto;
+import com.abiquo.scheduler.limit.LimitExceededException;
 import com.abiquo.server.core.cloud.VirtualDatacenter;
 import com.abiquo.server.core.cloud.VirtualDatacenterRep;
 import com.abiquo.server.core.common.Limit;
@@ -182,11 +183,14 @@ public class EnterpriseService extends DefaultApiService
         old.setVlansLimits(new Limit(dto.getVlansSoft(), dto.getVlansHard()));
         old.setPublicIPLimits(new Limit(dto.getPublicIpsSoft(), dto.getPublicIpsHard()));
 
-        isValidEnterprise(old);
-
+        isValidEnterprise(old);        
+        isValidEnterpriseLimit(old);
+               
         repo.update(old);
         return old;
     }
+
+    
 
     @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
     public void removeEnterprise(Integer id)
@@ -419,5 +423,11 @@ public class EnterpriseService extends DefaultApiService
         }
 
         flushErrors();
+    }
+    
+    protected void isValidEnterpriseLimit(Enterprise old)
+    {
+        // community dummy impl (no limit check)
+        
     }
 }
