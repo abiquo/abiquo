b6a8063be01335fc3715e0c60366d52657539943
Serafin Sedano <serafin.sedano@abiquo.com>
2011-05-11 11:27:21 +0200

    working

--- database/delta/1.8.0/kinton-delta-1_7_6-to-1_8_0.sql
+++ database/delta/1.8.0/kinton-delta-1_7_6-to-1_8_0.sql
@@ -22,6 +22,7 @@ alter table kinton.user modify user varchar(128) NOT NULL;
 alter table kinton.user add authType varchar(20) NOT NULL;
 alter table kinton.user modify column password varchar(32);
 update kinton.user set authtype = 'ABIQUO';
+alter table kinton.session modify user varchar(128) NOT NULL;
 
 
 -- [ABICLOUDPREMIUM 1615]  Accounting changes --
