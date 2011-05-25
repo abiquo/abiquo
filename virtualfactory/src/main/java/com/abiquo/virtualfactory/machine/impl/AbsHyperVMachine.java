ae3912bb97dfacf97571ed9321eec3d79641b780
danielestevez <daniel.estevez@abiquo.com>
2011-05-25 17:49:29 +0200

    VMachine files are stored in default Hyper-V directory configured

--- virtualfactory/src/main/java/com/abiquo/virtualfactory/machine/impl/AbsHyperVMachine.java
+++ virtualfactory/src/main/java/com/abiquo/virtualfactory/machine/impl/AbsHyperVMachine.java
@@ -164,7 +164,7 @@ public abstract class AbsHyperVMachine extends AbsVirtualMachine
         localRepositoryPath =
             localRepositoryPath.endsWith("\\") ? localRepositoryPath : localRepositoryPath + "\\";
 
-        String destinationTemp = localRepositoryPath.replace("\\", "\\\\");
+        String destinationTemp = localRepositoryPath;//.replace("\\", "\\\\"); // What's this for?
 
         destinationImagePath = destinationTemp + machineName + ".vhd";
 
@@ -610,12 +610,15 @@ public abstract class AbsHyperVMachine extends AbsVirtualMachine
 
             globalSettingDispatcher.put("ElementName", new JIVariant(new JIString(machineName)));
 
-            if (config.getVirtualDiskBase().getDiskType() == VirtualDiskType.STANDARD)
-            {
-                VirtualDisk diskBase = config.getVirtualDiskBase();
-                globalSettingDispatcher.put("ExternalDataRoot",
-                    new JIVariant(new JIString(getDatastore(diskBase))));
-            }
+            // ExternalDataRoot: The fully-qualified path to the root directory of an external data store. 
+            // This is set to default value to avoid problems with accessing from a networkdrive
+//            if (config.getVirtualDiskBase().getDiskType() == VirtualDiskType.STANDARD)
+//            {                
+//                VirtualDisk diskBase = config.getVirtualDiskBase();
+//                globalSettingDispatcher.put("ExternalDataRoot",
+////                    new JIVariant(new JIString(getDatastore(diskBase))));
+//                    new JIVariant(new JIString("C:\\")));
+//            }
 
             String globalSettingDataText =
                 globalSettingDispatcher.callMethodA("GetText_", new Object[] {new Integer(1)})[0]
@@ -638,12 +641,13 @@ public abstract class AbsHyperVMachine extends AbsVirtualMachine
 
     private String getDatastore(final VirtualDisk disk)
     {
-        String datastore = disk.getTargetDatastore();
-        if (!datastore.endsWith("/"))
-        {
-            datastore += "/";
-        }
-        return datastore;
+//        String datastore = disk.getTargetDatastore();
+//        if (!datastore.endsWith("/"))
+//        {
+//            datastore += "/";
+//        }
+//        return datastore;
+        return disk.getTargetDatastore();
     }
 
     /**
