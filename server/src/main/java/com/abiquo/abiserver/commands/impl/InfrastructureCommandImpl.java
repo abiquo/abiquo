0f5bca389bef4455c13d5f452d45fac72e2ec927
danielestevez <daniel.estevez@abiquo.com>
2011-05-26 17:26:42 +0200

    Fixed PhysicalMachine edition for IPMI fields

--- server/src/main/java/com/abiquo/abiserver/commands/impl/InfrastructureCommandImpl.java
+++ server/src/main/java/com/abiquo/abiserver/commands/impl/InfrastructureCommandImpl.java
@@ -1390,6 +1390,10 @@ public class InfrastructureCommandImpl extends BasicCommand implements Infrastru
             physicalMachineHb.setIdState(pm.getIdState());
             physicalMachineHb.getHypervisor().setIpService(ipService);
             physicalMachineHb.setVswitchName(pm.getVswitchName());
+            physicalMachineHb.setIpmiIp(pm.getIpmiIp());
+            physicalMachineHb.setIpmiPort(pm.getIpmiPort());
+            physicalMachineHb.setIpmiUser(pm.getIpmiUser());
+            physicalMachineHb.setIpmiPassword(pm.getIpmiPassword());
 
             // Updating virtual machines
             PhysicalMachineDAO pmDAO = factory.getPhysicalMachineDAO();
