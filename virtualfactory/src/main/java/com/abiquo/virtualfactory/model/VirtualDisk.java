cc58cd24852ba138361c77dac8664616eb06415f
danielestevez <daniel.estevez@abiquo.com>
2011-05-26 10:44:13 +0200

    Fixed control of imported VMachines in Hyper-V when parsing repository & imagepath

--- virtualfactory/src/main/java/com/abiquo/virtualfactory/model/VirtualDisk.java
+++ virtualfactory/src/main/java/com/abiquo/virtualfactory/model/VirtualDisk.java
@@ -212,9 +212,9 @@ public class VirtualDisk
             repository = location.substring(1, indexFinRepository);
             imagePath = location.substring(indexFinRepository + 1, location.length());
 
-            int indexRepositoryPath = location.indexOf(":"); // This is not valid for Hyper-V : check for repository == null is required
+            int indexRepositoryPath = location.indexOf(":"); // This is not valid for imported Hyper-V machines : repository == "null" in this case
 
-            if (indexRepositoryPath != -1 && repository != null) // FIXES call when using an imported VMachine
+            if (indexRepositoryPath != -1 && repository.length() > indexRepositoryPath) // FIXES call when using an imported VMachine -> Windows unit i.e. C:\ is detected
             {
                 repository = repository.substring(indexRepositoryPath, repository.length());
                 logger.debug("Using imagePath [{}] at repository [{}]", imagePath, repository);
