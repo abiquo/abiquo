8b16c9445b20ce837ac53b0d979764eebf5f0370
Enric Ruiz <morelies@gmail.com>
2011-06-03 13:32:48 +0200

    MOVED events on KVM cluster

--- vsm/src/main/java/com/abiquo/vsm/redis/pubsub/notifier/impl/LibvirtNotifier.java
+++ vsm/src/main/java/com/abiquo/vsm/redis/pubsub/notifier/impl/LibvirtNotifier.java
@@ -21,6 +21,16 @@
 
 package com.abiquo.vsm.redis.pubsub.notifier.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.abiquo.commons.amqp.impl.vsm.domain.VirtualSystemEvent;
+import com.abiquo.vsm.events.VMEventType;
+import com.abiquo.vsm.model.PhysicalMachine;
+import com.abiquo.vsm.model.VirtualMachine;
 import com.abiquo.vsm.redis.pubsub.notifier.GenericNotifier;
 
 /**
@@ -30,5 +40,47 @@ import com.abiquo.vsm.redis.pubsub.notifier.GenericNotifier;
  */
 public class LibvirtNotifier extends GenericNotifier
 {
+    private final static Logger logger = LoggerFactory.getLogger(ESXiNotifier.class);
 
+    @Override
+    public List<VirtualSystemEvent> processEvent(final VirtualMachine virtualMachine,
+        final PhysicalMachine machine, final VMEventType event)
+    {
+        logger.trace(String.format("Processing %s %s event from machine %s", virtualMachine
+            .getName(), event.name(), machine.getAddress()));
+
+        List<VirtualSystemEvent> notifications = new ArrayList<VirtualSystemEvent>();
+
+        switch (event)
+        {
+            case CREATED:
+                notifications.addAll(deduceMoveEvent(virtualMachine, machine, event));
+                break;
+
+            case POWER_ON:
+            case POWER_OFF:
+            case PAUSED:
+            case RESUMED:
+            case DESTROYED:
+
+                if (samePhysicalMachineAddress(virtualMachine.getPhysicalMachine(), machine)
+                    && !alreadyNotified(virtualMachine, event))
+                {
+                    notifications.add(buildVirtualSystemEvent(virtualMachine, event));
+                }
+
+                break;
+
+            default:
+                break;
+        }
+
+        return notifications;
+    }
+
+    protected boolean samePhysicalMachineAddress(final PhysicalMachine one,
+        final PhysicalMachine other)
+    {
+        return one.getAddress().equals(other.getAddress());
+    }
 }
