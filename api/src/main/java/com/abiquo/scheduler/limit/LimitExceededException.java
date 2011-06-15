816a94fb959d55b5e6b7428035656ca3ea6267fc
Alessia Prete <alessia.prete@abiquo.com>
2011-06-15 16:45:52 +0200

    ABICLOUDPREMIUM-1479 Tracer -added unit en MB

--- api/src/main/java/com/abiquo/scheduler/limit/LimitExceededException.java
+++ api/src/main/java/com/abiquo/scheduler/limit/LimitExceededException.java
@@ -46,9 +46,9 @@ public class LimitExceededException extends AllocatorException
 
     private String entityId;
 
-    public LimitExceededException(Map<LimitResource, LimitStatus> resourcesStatus,
-        DefaultEntityWithLimits entity, VirtualMachineRequirements requirements,
-        DefaultEntityCurrentUsed actual, String entityId)
+    public LimitExceededException(final Map<LimitResource, LimitStatus> resourcesStatus,
+        final DefaultEntityWithLimits entity, final VirtualMachineRequirements requirements,
+        final DefaultEntityCurrentUsed actual, final String entityId)
     {
         super("Limit exceeded");
         this.entity = entity;
@@ -88,34 +88,40 @@ public class LimitExceededException extends AllocatorException
     {
         return String.format(
             //
-            "%s ; \n" + //
-                "USED\t       CPU=%d \t RAM=%d \t HD=%d \t Storage=%d \t VLAN=%d \t IP=%d \t ; \n" + //
-                "REQUIRED\t   CPU=%d \t RAM=%d \t HD=%d \t Storage=%d \t VLAN=%d \t IP=%d \t ; \n" + //
-                "SOFT LIMIT\t CPU=%d \t RAM=%d \t HD=%d \t Storage=%d \t VLAN=%d \t IP=%d \t ; \n" + //
-                "HARD LIMIT\t CPU=%d \t RAM=%d \t HD=%d \t Storage=%d \t VLAN=%d \t IP=%d \t ; \n" + //
+            "%s ; \n"
+                + //
+                "USED\t       CPU=%d \t RAM=%d MB \t HD=%d MB \t Storage=%d MB \t VLAN=%d \t IP=%d \t ; \n"
+                + //
+                "REQUIRED\t   CPU=%d \t RAM=%d MB \t HD=%d MB \t Storage=%d MB \t VLAN=%d \t IP=%d \t ; \n"
+                + //
+                "SOFT LIMIT\t CPU=%d \t RAM=%d MB \t HD=%d MB \t Storage=%d MB \t VLAN=%d \t IP=%d \t ; \n"
+                + //
+                "HARD LIMIT\t CPU=%d \t RAM=%d MB \t HD=%d MB \t Storage=%d MB \t VLAN=%d \t IP=%d \t ; \n"
+                + //
                 "STATUS\t     CPU=%s \t RAM=%s \t HD=%s \t Storage=%s \t VLAN=%s \t IP=%s \t ; \n", //
             entityId, //
             actual.getCpu(),
             actual.getRamInMb(),
             actual.getHdInMb(),
-            actual.getStorage(),
+            (actual.getStorage() / 1048576), // en MB
             actual.getVlanCount(),
             actual.getPublicIp(), //
             requir.getCpu(),
             requir.getRam(),
             requir.getHd(),
-            requir.getStorage(),
+            (requir.getStorage() / 1048576),// en MB
             requir.getPublicVLAN(),
             requir.getPublicIP(), //
             entity.getCpuCountSoftLimit(),
             entity.getRamSoftLimitInMb(),
             entity.getHdSoftLimitInMb(),
-            entity.getStorageSoft(),
+            (entity.getStorageSoft() / 1048576),// en MB
             entity.getVlanSoft(),
             entity.getPublicIpsSoft(), //
-            entity.getCpuCountHardLimit(), entity.getRamHardLimitInMb(),
+            entity.getCpuCountHardLimit(),
+            entity.getRamHardLimitInMb(),
             entity.getHdHardLimitInMb(),
-            entity.getStorageHard(),
+            (entity.getStorageHard() / 1048576),// en MB
             entity.getVlanHard(),
             entity.getPublicIpsHard(), //
             status.get(LimitResource.CPU),
