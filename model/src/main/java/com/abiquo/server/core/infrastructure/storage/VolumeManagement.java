0dd06970c08b491bb0c47ea2ac403eca83534d87
Vivien Mahe <vivien.mahe@abiquo.com>
2011-07-01 18:38:25 +0200

    Fixed a bug in volumes list sort

--- model/src/main/java/com/abiquo/server/core/infrastructure/storage/VolumeManagement.java
+++ model/src/main/java/com/abiquo/server/core/infrastructure/storage/VolumeManagement.java
@@ -305,7 +305,7 @@ public class VolumeManagement extends RasdManagement
     public static enum OrderByEnum
     {
         NAME("elementname", "vol.rasd.elementName"), ID("idman", "vol.id"), VIRTUALDATACENTER(
-            "vdcname", "vol.virtualDatacenter.name"), VIRTUALMACHINE("vmname", "vol.name"), VIRTUALAPPLIANCE(
+            "vdcname", "vol.virtualDatacenter.name"), VIRTUALMACHINE("vmname", "vm.name"), VIRTUALAPPLIANCE(
             "vaname", "vapp.name"), TIER("tier", "vol.storagePool.tier.name"), TOTALSIZE("size",
             "vol.rasd.limit"), AVAILABLESIZE("available", "vol.rasd.reservation"), USEDSIZE("used",
             "vol.usedSizeInMB"), STATE("state", "vol.state");
