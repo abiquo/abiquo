e03bb5cfad3ab38a0baa28af72a7a2ec30a92cb1
Susana Acedo <susana.acedo@abiquo.com>
2011-07-18 16:22:51 +0200

    pricing rep changes

--- model/src/main/java/com/abiquo/server/core/pricing/PricingRep.java
+++ model/src/main/java/com/abiquo/server/core/pricing/PricingRep.java
@@ -24,9 +24,11 @@ package com.abiquo.server.core.pricing;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.abiquo.server.core.common.DefaultRepBase;
 
+@Repository
 public class PricingRep extends DefaultRepBase
 {
 
