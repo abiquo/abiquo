c567d888d1548cfed5eb5e2428fc39689afd6184
Susana Acedo <susana.acedo@abiquo.com>
2011-07-18 15:14:53 +0200

    pricingRep changes

--- model/src/main/java/com/abiquo/server/core/pricing/PricingRep.java
+++ model/src/main/java/com/abiquo/server/core/pricing/PricingRep.java
@@ -26,22 +26,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.abiquo.server.core.common.DefaultRepBase;
-import com.abiquo.server.core.infrastructure.DatacenterDAO;
 
 public class PricingRep extends DefaultRepBase
 {
 
     @Autowired
-    private DatacenterDAO dao;
-
-    @Autowired
-    private PricingTemplateDAO pricingDao;
-
-    @Autowired
     private CurrencyDAO currencyDao;
-
-    @Autowired
-    private CostCodeDAO costCodeDao;
 
     public PricingRep()
     {
