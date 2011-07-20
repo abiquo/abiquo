732cc6530d0e6648e28e8634085750ec9cfc5a5a
Sergi Castro <sergi.castro@abiquo.com>
2011-07-20 11:42:54 +0200

    Error message changed

--- api/src/main/java/com/abiquo/api/exceptions/APIError.java
+++ api/src/main/java/com/abiquo/api/exceptions/APIError.java
@@ -165,7 +165,7 @@ public enum APIError
         "Cannot delete a role with associated User"), DELETE_ERROR_WITH_ROLE_LDAP("ROLE-5",
         "Cannot delete a role with associated RoleLdap"), DUPLICATED_ROLE_NAME_ENT("ROLE-6",
         "Cannot create a role with the same name of an existing role for the same enterprise"), DUPLICATED_ROLE_NAME_GEN(
-        "ROLE-7", "Cannot create a generic role with the same name of an existing generic role"),
+        "ROLE-7", "Cannot create a golbal role with the same name of an existing global role"),
 
     // PRIVILEGE
     NON_EXISTENT_PRIVILEGE("PRIVILEGE-0", "The requested privilege does not exist"),
@@ -368,8 +368,8 @@ public enum APIError
         // Outputs all errors in wiki table format
         for (APIError error : errors)
         {
-            System.out.println(String.format("| %s | %s | %s |", error.code, error.message, error
-                .name()));
+            System.out.println(String.format("| %s | %s | %s |", error.code, error.message,
+                error.name()));
         }
     }
 
