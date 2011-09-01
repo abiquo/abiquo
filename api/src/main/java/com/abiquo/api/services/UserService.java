f61803a9e2e78b678aae1ed455b4dc3124a98c06
Susana Acedo <susana.acedo@abiquo.com>
2011-09-01 12:00:27 +0200

    ABICLOUDPREMIUM-2237

--- api/src/main/java/com/abiquo/api/services/UserService.java
+++ api/src/main/java/com/abiquo/api/services/UserService.java
@@ -349,6 +349,14 @@ public class UserService extends DefaultApiService
     {
         User user = getUser(id);
 
+        // user can not delete himself
+        User logged = getCurrentUser();
+        if (logged.getId() == user.getId())
+        {
+            addConflictErrors(APIError.USER_DELETING_HIMSELF);
+            flushErrors();
+        }
+
         checkEnterpriseAdminCredentials(user.getEnterprise());
 
         // Cloud Admins should only be editable by other Cloud Admins
@@ -444,7 +452,7 @@ public class UserService extends DefaultApiService
         // if ((role == Role.Type.ENTERPRISE_ADMIN && !enterprise.equals(user.getEnterprise()))
         // || role == Role.Type.USER)
 
-        if ((securityService.isEnterpriseAdmin() && !sameEnterprise)
+        if (securityService.isEnterpriseAdmin() && !sameEnterprise
             || securityService.isStandardUser())
         {
             throw new AccessDeniedException("");
@@ -457,7 +465,9 @@ public class UserService extends DefaultApiService
         for (User user : users)
         {
             if (user.getRole().isBlocked())
+            {
                 return user.getRole().getName().toString();
+            }
         }
         return "";
     }
@@ -473,8 +483,8 @@ public class UserService extends DefaultApiService
         // if ((role == Role.Type.ENTERPRISE_ADMIN && !enterprise.equals(user.getEnterprise()))
         // || (role == Role.Type.USER && user.getId() != selfUser.getId()))
 
-        if ((securityService.isEnterpriseAdmin() && !sameEnterprise)
-            || (securityService.isStandardUser() && !sameUser))
+        if (securityService.isEnterpriseAdmin() && !sameEnterprise
+            || securityService.isStandardUser() && !sameUser)
         {
             throw new AccessDeniedException("");
         }
@@ -488,12 +498,11 @@ public class UserService extends DefaultApiService
         // Role.Type role = user.getRole().getType();
         // if ((role == Role.Type.ENTERPRISE_ADMIN || role == Role.Type.USER) && !sameEnterprise)
         if (!sameEnterprise
-            && (!securityService.hasPrivilege(SecurityService.USERS_MANAGE_OTHER_ENTERPRISES)
-                && !securityService
-                    .hasPrivilege(SecurityService.USERS_MANAGE_ROLES_OTHER_ENTERPRISES)
-                && !securityService.hasPrivilege(SecurityService.ENTERPRISE_ENUMERATE)
-                && !securityService.hasPrivilege(SecurityService.ENTRPRISE_ADMINISTER_ALL) && !securityService
-                .hasPrivilege(SecurityService.PHYS_DC_ENUMERATE)))
+            && !securityService.hasPrivilege(SecurityService.USERS_MANAGE_OTHER_ENTERPRISES)
+            && !securityService.hasPrivilege(SecurityService.USERS_MANAGE_ROLES_OTHER_ENTERPRISES)
+            && !securityService.hasPrivilege(SecurityService.ENTERPRISE_ENUMERATE)
+            && !securityService.hasPrivilege(SecurityService.ENTRPRISE_ADMINISTER_ALL)
+            && !securityService.hasPrivilege(SecurityService.PHYS_DC_ENUMERATE))
         {
             throw new AccessDeniedException("Missing privilege to get info from other enterprises");
         }
@@ -505,7 +514,7 @@ public class UserService extends DefaultApiService
         boolean sameEnterprise = enterprise.getId().equals(user.getEnterprise().getId());
 
         if (!sameEnterprise
-            && (!securityService.hasPrivilege(SecurityService.ENTRPRISE_ADMINISTER_ALL)))
+            && !securityService.hasPrivilege(SecurityService.ENTRPRISE_ADMINISTER_ALL))
         {
             throw new AccessDeniedException("Missing privilege to manage info from other enterprises");
         }
@@ -513,7 +522,7 @@ public class UserService extends DefaultApiService
 
     private Boolean emailIsValid(final String email)
     {
-        if ((email != null) && (!email.isEmpty()))
+        if (email != null && !email.isEmpty())
         {
             final Pattern pattern;
             final Matcher matchers;
@@ -525,6 +534,8 @@ public class UserService extends DefaultApiService
             return matchers.matches();
         }
         else
+        {
             return true;
+        }
     }
 }
