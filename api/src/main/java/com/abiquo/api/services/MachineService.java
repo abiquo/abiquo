dfb5733cfac54168d9e53e1d907f89acf7d2ea3b
Serafin Sedano <serafin.sedano@abiquo.com>
2011-09-08 10:06:02 +0200

    Fixed an error in writing.

--- api/src/main/java/com/abiquo/api/services/MachineService.java
+++ api/src/main/java/com/abiquo/api/services/MachineService.java
@@ -149,9 +149,9 @@ public class MachineService extends DefaultApiService
     {
         Machine old = getMachine(machineId);
 
-        if (!old.getBelongsToManagedRack())
+        if (old.getBelongsToManagedRack())
         {
-            if (old.getName().equalsIgnoreCase(machineDto.getName()))
+            if (!old.getName().equalsIgnoreCase(machineDto.getName()))
             {
                 addValidationErrors(APIError.MANAGED_MACHINE_CANNOT_CHANGE_NAME);
                 flushErrors();
