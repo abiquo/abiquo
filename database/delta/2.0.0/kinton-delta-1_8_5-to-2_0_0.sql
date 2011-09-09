6e8f7a3cf3057351ef995f00b34538441a9b0ba8
Susana Acedo <susana.acedo@abiquo.com>
2011-09-09 14:09:02 +0200

    rename kinton delta

--- database/delta/2.0.0/kinton-delta-1_8_5-to-2_0_0.sql
+++ database/delta/2.0.0/kinton-delta-1_8_5-to-2_0_0.sql
@@ -1,3 +1,16 @@
+
+-- WARNING
+-- Please maintain order of delta when merging or adding new lines
+-- 1st -> alter existing schema tables
+-- 2st -> new created schema tables
+-- 3rd -> insert/update data
+-- 4th -> Triggers
+-- 5th -> SQL Procedures
+
+
+-- [ABICLOUDPREMIUM-2057]
+UPDATE kinton.metering SET actionperformed="VAPP_INSTANCE" WHERE actionperformed="VAPP_BUNDLE";
+
 -- ---------------------------------------------- --
 --                 TABLE DROP                     --
 -- ---------------------------------------------- --
@@ -99,6 +112,15 @@ CREATE TABLE `kinton`.`pricingTier` (
 -- ---------------------------------------------- --
 --         CONSTRAINTS (alter table, etc)         --
 -- ---------------------------------------------- --
+alter table repository modify URL varchar(255);
+-- [UCS]
+ALTER TABLE `kinton`.`physicalmachine` MODIFY COLUMN `vswitchName` varchar(200) NOT NULL;
+ALTER TABLE `kinton`.`vlan_network` ADD COLUMN `networktype` varchar(15) DEFAULT 'internal';
+ALTER TABLE `kinton`.`virtualdatacenter` ADD COLUMN `default_vlan_network_id` int(11) unsigned DEFAULT NULL; 
+ALTER TABLE `kinton`.`virtualdatacenter` ADD CONSTRAINT `virtualDataCenter_FK7` FOREIGN KEY (`default_vlan_network_id`) REFERENCES `vlan_network` (`vlan_network_id`),
+ALTER TABLE `kinton`.`enterprise_limits_by_datacenter` ADD COLUMN `default_vlan_network_id` int(11) unsigned DEFAULT NULL; 
+ALTER TABLE `kinton`.`enterprise_limits_by_datacenter` ADD CONSTRAINT `enterprise_FK7` FOREIGN KEY (`default_vlan_network_id`) REFERENCES `vlan_network` (`vlan_network_id`),
+ALTER TABLE `kinton`.`ip_pool_management` ADD COLUMN `available` boolean NOT NULL default 1; 
 
 -- PRICING --
 -- ADD THE COLUMN ID_PRICING TO ENTERPRISE --
@@ -109,6 +131,9 @@ ALTER TABLE `kinton`.`enterprise` ADD CONSTRAINT `enterprise_pricing_FK` FOREIGN
 -- ---------------------------------------------- --
 --   DATA CHANGES (insert, update, delete, etc)   --
 -- ---------------------------------------------- --
+-- First I need to update some rows before to delete the `default_network` field
+UPDATE `kinton`.`virtualdatacenter` vdc, `kinton`.`vlan_network` v set vdc.default_vlan_network_id = v.vlan_network_id WHERE vdc.networktypeID = v.network_id and v.default_network = 1;
+ALTER TABLE `kinton`.`vlan_network` DROP COLUMN `default_network`;
 
 -- PRICING --
 -- Dumping data for table `kinton`.`privilege`
