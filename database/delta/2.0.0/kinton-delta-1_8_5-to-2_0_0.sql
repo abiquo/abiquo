4cf270fdffc7c48259e36c2e0aa26587f7b8afb6
Susana Acedo <susana.acedo@abiquo.com>
2011-09-12 10:45:59 +0200

    virtualImage-costcode changed

--- database/delta/2.0.0/kinton-delta-1_8_5-to-2_0_0.sql
+++ database/delta/2.0.0/kinton-delta-1_8_5-to-2_0_0.sql
@@ -105,7 +105,7 @@ CREATE TABLE `kinton`.`pricingTier` (
 ALTER TABLE `kinton`.`enterprise` ADD COLUMN `idPricingTemplate` int(10) unsigned DEFAULT NULL;
 ALTER TABLE `kinton`.`enterprise` ADD CONSTRAINT `enterprise_pricing_FK` FOREIGN KEY (`idPricingTemplate`) REFERENCES `kinton`.`pricing_template` (`idPricingTemplate`);
 
-
+ALTER TABLE `kinton`.`virtualimage` MODIFY COLUMN `cost_code` int(4) DEFAULT 0;
 -- ---------------------------------------------- --
 --   DATA CHANGES (insert, update, delete, etc)   --
 -- ---------------------------------------------- --
@@ -162,9 +162,147 @@ UNLOCK TABLES;
 -- ---------------------------------------------- --
 
 
+/* cost code has changed from string to int */
+DROP TRIGGER IF EXISTS `kinton`.`update_virtualmachine_update_stats`;
+CREATE TRIGGER `kinton`.`update_virtualmachine_update_stats` AFTER UPDATE ON `kinton`.`virtualmachine`
+    FOR EACH ROW BEGIN
+        DECLARE idDataCenterObj INTEGER;
+        DECLARE idVirtualAppObj INTEGER;
+        DECLARE idVirtualDataCenterObj INTEGER;
+        DECLARE costCodeObj int(4);
+    -- For debugging purposes only
+        -- INSERT INTO debug_msg (msg) VALUES (CONCAT('UPDATE: ', OLD.idType, NEW.idType, OLD.state, NEW.state));   
+        IF (@DISABLE_STATS_TRIGGERS IS NULL) THEN   
+        --  Updating enterprise_resources_stats: VCPU Used, Memory Used, Local Storage Used
+        IF OLD.idHypervisor IS NULL OR (OLD.idHypervisor != NEW.idHypervisor) THEN
+            SELECT pm.idDataCenter INTO idDataCenterObj
+            FROM hypervisor hy, physicalmachine pm
+            WHERE NEW.idHypervisor=hy.id
+            AND hy.idPhysicalMachine=pm.idPhysicalMachine;
+        ELSE 
+            SELECT pm.idDataCenter INTO idDataCenterObj
+            FROM hypervisor hy, physicalmachine pm
+            WHERE OLD.idHypervisor=hy.id
+            AND hy.idPhysicalMachine=pm.idPhysicalMachine;
+        END IF;     
+        --
+        SELECT n.idVirtualApp, vapp.idVirtualDataCenter INTO idVirtualAppObj, idVirtualDataCenterObj
+        FROM nodevirtualimage nvi, node n, virtualapp vapp
+        WHERE NEW.idVM = nvi.idVM
+        AND nvi.idNode = n.idNode
+        AND vapp.idVirtualApp = n.idVirtualApp;   
 
-
-
+    IF NEW.idType = 1 AND OLD.idType = 0 THEN
+        -- Imported !!!
+        UPDATE IGNORE cloud_usage_stats SET vMachinesTotal = vMachinesTotal+1
+                WHERE idDataCenter = idDataCenterObj;
+                UPDATE IGNORE vapp_enterprise_stats SET vmCreated = vmCreated+1
+                WHERE idVirtualApp = idVirtualAppObj;
+                UPDATE IGNORE vdc_enterprise_stats SET vmCreated = vmCreated+1
+                WHERE idVirtualDataCenter = idVirtualDataCenterObj;
+        IF NEW.state = "RUNNING" THEN   
+            UPDATE IGNORE vapp_enterprise_stats SET vmActive = vmActive+1
+                WHERE idVirtualApp = idVirtualAppObj;
+                UPDATE IGNORE vdc_enterprise_stats SET vmActive = vmActive+1
+                WHERE idVirtualDataCenter = idVirtualDataCenterObj;
+                UPDATE IGNORE cloud_usage_stats SET vMachinesRunning = vMachinesRunning+1
+                WHERE idDataCenter = idDataCenterObj;       
+                UPDATE IGNORE enterprise_resources_stats 
+                    SET vCpuUsed = vCpuUsed + NEW.cpu,
+                        memoryUsed = memoryUsed + NEW.ram,
+                        localStorageUsed = localStorageUsed + NEW.hd
+                WHERE idEnterprise = NEW.idEnterprise;
+                UPDATE IGNORE dc_enterprise_stats 
+                SET     vCpuUsed = vCpuUsed + NEW.cpu,
+                    memoryUsed = memoryUsed + NEW.ram,
+                    localStorageUsed = localStorageUsed + NEW.hd
+                WHERE idEnterprise = NEW.idEnterprise AND idDataCenter = idDataCenterObj;
+                UPDATE IGNORE vdc_enterprise_stats 
+                SET     vCpuUsed = vCpuUsed + NEW.cpu,
+                    memoryUsed = memoryUsed + NEW.ram,
+                    localStorageUsed = localStorageUsed + NEW.hd
+                WHERE idVirtualDataCenter = idVirtualDataCenterObj; 
+        END IF;
+    ELSEIF NEW.idType = 1 AND (NEW.state != OLD.state) THEN
+            IF NEW.state = "RUNNING" THEN 
+                -- New Active
+                UPDATE IGNORE vapp_enterprise_stats SET vmActive = vmActive+1
+                WHERE idVirtualApp = idVirtualAppObj;
+                UPDATE IGNORE vdc_enterprise_stats SET vmActive = vmActive+1
+                WHERE idVirtualDataCenter = idVirtualDataCenterObj;
+                UPDATE IGNORE cloud_usage_stats SET vMachinesRunning = vMachinesRunning+1
+                WHERE idDataCenter = idDataCenterObj;       
+                UPDATE IGNORE enterprise_resources_stats 
+                    SET vCpuUsed = vCpuUsed + NEW.cpu,
+                        memoryUsed = memoryUsed + NEW.ram,
+                        localStorageUsed = localStorageUsed + NEW.hd
+                WHERE idEnterprise = NEW.idEnterprise;
+                UPDATE IGNORE dc_enterprise_stats 
+                SET     vCpuUsed = vCpuUsed + NEW.cpu,
+                    memoryUsed = memoryUsed + NEW.ram,
+                    localStorageUsed = localStorageUsed + NEW.hd
+                WHERE idEnterprise = NEW.idEnterprise AND idDataCenter = idDataCenterObj;
+                UPDATE IGNORE vdc_enterprise_stats 
+                SET     vCpuUsed = vCpuUsed + NEW.cpu,
+                    memoryUsed = memoryUsed + NEW.ram,
+                    localStorageUsed = localStorageUsed + NEW.hd
+                WHERE idVirtualDataCenter = idVirtualDataCenterObj;
+-- cloud_usage_stats Used Stats (vCpuUsed, vMemoryUsed, vStorageUsed) are updated from update_physical_machine_update_stats trigger
+            ELSEIF OLD.state = "RUNNING" THEN           
+                -- Active Out
+                UPDATE IGNORE vapp_enterprise_stats SET vmActive = vmActive-1
+                WHERE idVirtualApp = idVirtualAppObj;
+                UPDATE IGNORE vdc_enterprise_stats SET vmActive = vmActive-1
+                WHERE idVirtualDataCenter = idVirtualDataCenterObj;
+                UPDATE IGNORE cloud_usage_stats SET vMachinesRunning = vMachinesRunning-1
+                WHERE idDataCenter = idDataCenterObj;
+                UPDATE IGNORE enterprise_resources_stats 
+                    SET vCpuUsed = vCpuUsed - NEW.cpu,
+                        memoryUsed = memoryUsed - NEW.ram,
+                        localStorageUsed = localStorageUsed - NEW.hd
+                WHERE idEnterprise = NEW.idEnterprise;
+                UPDATE IGNORE dc_enterprise_stats 
+                SET     vCpuUsed = vCpuUsed - NEW.cpu,
+                    memoryUsed = memoryUsed - NEW.ram,
+                    localStorageUsed = localStorageUsed - NEW.hd
+                WHERE idEnterprise = NEW.idEnterprise AND idDataCenter = idDataCenterObj;
+                UPDATE IGNORE vdc_enterprise_stats 
+                SET     vCpuUsed = vCpuUsed - NEW.cpu,
+                    memoryUsed = memoryUsed - NEW.ram,
+                    localStorageUsed = localStorageUsed - NEW.hd
+                WHERE idVirtualDataCenter = idVirtualDataCenterObj; 
+-- cloud_usage_stats Used Stats (vCpuUsed, vMemoryUsed, vStorageUsed) are updated from update_physical_machine_update_stats trigger
+            END IF;     
+            IF OLD.state = "NOT_DEPLOYED" OR OLD.state = "UNKNOWN"  THEN -- OR OLD.idType != NEW.idType
+                -- VMachine Deployed or VMachine imported
+                UPDATE IGNORE cloud_usage_stats SET vMachinesTotal = vMachinesTotal+1
+                WHERE idDataCenter = idDataCenterObj;
+                UPDATE IGNORE vapp_enterprise_stats SET vmCreated = vmCreated+1
+                WHERE idVirtualApp = idVirtualAppObj;
+                UPDATE IGNORE vdc_enterprise_stats SET vmCreated = vmCreated+1
+                WHERE idVirtualDataCenter = idVirtualDataCenterObj;
+            ELSEIF NEW.state = "NOT_DEPLOYED" OR NEW.state = "CRASHED" OR (NEW.state = "UNKNOWN" AND OLD.state != "CRASHED") THEN 
+                -- VMachine Undeployed
+                UPDATE IGNORE cloud_usage_stats SET vMachinesTotal = vMachinesTotal-1
+                WHERE idDataCenter = idDataCenterObj;
+                UPDATE IGNORE vapp_enterprise_stats SET vmCreated = vmCreated-1
+                WHERE idVirtualApp = idVirtualAppObj;
+                UPDATE IGNORE vdc_enterprise_stats SET vmCreated = vmCreated-1
+                WHERE idVirtualDataCenter = idVirtualDataCenterObj;
+            END IF;         
+        END IF;
+        --
+        SELECT IF(vi.cost_code IS NULL, 0, vi.cost_code) INTO costCodeObj
+        FROM virtualimage vi
+        WHERE vi.idImage = NEW.idImage;
+        -- Register Accounting Events
+        IF EXISTS( SELECT * FROM `information_schema`.ROUTINES WHERE ROUTINE_SCHEMA='kinton' AND ROUTINE_TYPE='PROCEDURE' AND ROUTINE_NAME='AccountingVMRegisterEvents' ) THEN
+             IF EXISTS(SELECT * FROM virtualimage vi WHERE vi.idImage=NEW.idImage AND vi.idRepository IS NOT NULL) THEN 
+              CALL AccountingVMRegisterEvents(NEW.idVM, NEW.idType, OLD.state, NEW.state, NEW.ram, NEW.cpu, NEW.hd, costCodeObj);
+             END IF;              
+        END IF;
+      END IF;
+    END;
 
 
 
