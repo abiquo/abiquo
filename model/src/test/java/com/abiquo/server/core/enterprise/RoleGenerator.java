2bcf9b2e0c5fdf12ee74708fba89e1627fa7c2cd
David Lopez <david.lopez@abiquo.com>
2011-10-13 10:23:01 +0200

    [ABICLOUDPREMIUM-1292] Enterprise admin can modify or remove cloud admin - FIXED. Moved privilege role management to a new enum.

--- model/src/test/java/com/abiquo/server/core/enterprise/RoleGenerator.java
+++ model/src/test/java/com/abiquo/server/core/enterprise/RoleGenerator.java
@@ -24,6 +24,7 @@ package com.abiquo.server.core.enterprise;
 import java.util.Collection;
 import java.util.List;
 
+import com.abiquo.model.enumerator.Privileges;
 import com.abiquo.server.core.common.DefaultEntityGenerator;
 import com.softwarementors.commons.test.SeedGenerator;
 import com.softwarementors.commons.testng.AssertEx;
@@ -33,29 +34,6 @@ public class RoleGenerator extends DefaultEntityGenerator<Role>
     PrivilegeGenerator privilegeGenerator;
 
     EnterpriseGenerator enterpriseGenerator;
-
-    public static final String ENTRPRISE_ADMINISTER_ALL = "ENTERPRISE_ADMINISTER_ALL";
-
-    public static final String USERS_MANAGE_OTHER_ENTERPRISES = "USERS_MANAGE_OTHER_ENTERPRISES";
-
-    public static final String USERS_VIEW = "USERS_VIEW";
-
-    public static final String USERS_VIEW_PRIVILEGES = "USERS_VIEW_PRIVILEGES";
-
-    public static final String USERS_MANAGE_LDAP_GROUP = "USERS_MANAGE_LDAP_GROUP";
-
-    public static final String USERS_MANAGE_USERS = "USERS_MANAGE_USERS";
-
-    public static final String USERS_MANAGE_ROLES = "USERS_MANAGE_ROLES";
-
-    public static final String USERS_MANAGE_ROLES_OTHER_ENTERPRISES =
-        "USERS_MANAGE_ROLES_OTHER_ENTERPRISES";
-
-    public static final String USERS_MANAGE_SYSTEM_ROLES = "USERS_MANAGE_SYSTEM_ROLES";
-
-    public static final String USERS_PROHIBIT_VDC_RESTRICTION = "USERS_PROHIBIT_VDC_RESTRICTION";
-
-    public static final String VDC_ENUMERATE = "VDC_ENUMERATE";
 
     public RoleGenerator(final SeedGenerator seed)
     {
@@ -81,42 +59,42 @@ public class RoleGenerator extends DefaultEntityGenerator<Role>
 
     public Role createInstanceSysAdmin()
     {
-        Privilege p1 = new Privilege(USERS_MANAGE_OTHER_ENTERPRISES);
-        Privilege p3 = new Privilege(USERS_VIEW);
-        Privilege p4 = new Privilege(USERS_VIEW_PRIVILEGES);
-        Privilege p5 = new Privilege(USERS_MANAGE_USERS);
-        Privilege p6 = new Privilege(USERS_MANAGE_ROLES_OTHER_ENTERPRISES);
-        Privilege p7 = new Privilege(USERS_MANAGE_SYSTEM_ROLES);
-        Privilege p8 = new Privilege(USERS_PROHIBIT_VDC_RESTRICTION);
-        Privilege p9 = new Privilege(USERS_MANAGE_LDAP_GROUP);
-        Privilege p10 = new Privilege(ENTRPRISE_ADMINISTER_ALL);
-        Privilege p11 = new Privilege(VDC_ENUMERATE);
-        Privilege p12 = new Privilege(USERS_MANAGE_ROLES);
+        Privilege p1 = new Privilege(Privileges.USERS_MANAGE_OTHER_ENTERPRISES);
+        Privilege p3 = new Privilege(Privileges.USERS_VIEW);
+        Privilege p4 = new Privilege(Privileges.USERS_VIEW_PRIVILEGES);
+        Privilege p5 = new Privilege(Privileges.USERS_MANAGE_USERS);
+        Privilege p6 = new Privilege(Privileges.USERS_MANAGE_ROLES_OTHER_ENTERPRISES);
+        Privilege p7 = new Privilege(Privileges.USERS_MANAGE_SYSTEM_ROLES);
+        Privilege p8 = new Privilege(Privileges.USERS_PROHIBIT_VDC_RESTRICTION);
+        Privilege p9 = new Privilege(Privileges.USERS_MANAGE_LDAP_GROUP);
+        Privilege p10 = new Privilege(Privileges.ENTRPRISE_ADMINISTER_ALL);
+        Privilege p11 = new Privilege(Privileges.VDC_ENUMERATE);
+        Privilege p12 = new Privilege(Privileges.USERS_MANAGE_ROLES);
         return createInstance(p1, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12);
     }
 
     public Role createInstanceSysAdmin(final String name)
     {
-        Privilege p1 = new Privilege(USERS_MANAGE_OTHER_ENTERPRISES);
-        Privilege p3 = new Privilege(USERS_VIEW);
-        Privilege p4 = new Privilege(USERS_VIEW_PRIVILEGES);
-        Privilege p5 = new Privilege(USERS_MANAGE_USERS);
-        Privilege p6 = new Privilege(USERS_MANAGE_ROLES_OTHER_ENTERPRISES);
-        Privilege p7 = new Privilege(USERS_MANAGE_SYSTEM_ROLES);
-        Privilege p8 = new Privilege(USERS_PROHIBIT_VDC_RESTRICTION);
-        Privilege p9 = new Privilege(USERS_MANAGE_LDAP_GROUP);
-        Privilege p10 = new Privilege(ENTRPRISE_ADMINISTER_ALL);
-        Privilege p11 = new Privilege(VDC_ENUMERATE);
+        Privilege p1 = new Privilege(Privileges.USERS_MANAGE_OTHER_ENTERPRISES);
+        Privilege p3 = new Privilege(Privileges.USERS_VIEW);
+        Privilege p4 = new Privilege(Privileges.USERS_VIEW_PRIVILEGES);
+        Privilege p5 = new Privilege(Privileges.USERS_MANAGE_USERS);
+        Privilege p6 = new Privilege(Privileges.USERS_MANAGE_ROLES_OTHER_ENTERPRISES);
+        Privilege p7 = new Privilege(Privileges.USERS_MANAGE_SYSTEM_ROLES);
+        Privilege p8 = new Privilege(Privileges.USERS_PROHIBIT_VDC_RESTRICTION);
+        Privilege p9 = new Privilege(Privileges.USERS_MANAGE_LDAP_GROUP);
+        Privilege p10 = new Privilege(Privileges.ENTRPRISE_ADMINISTER_ALL);
+        Privilege p11 = new Privilege(Privileges.VDC_ENUMERATE);
         return createInstance(name, p1, p3, p4, p5, p6, p7, p8, p9, p10, p11);
     }
 
     public Role createInstanceEnterprisAdmin()
     {
-        Privilege p3 = new Privilege(USERS_VIEW);
-        Privilege p4 = new Privilege(USERS_VIEW_PRIVILEGES);
-        Privilege p5 = new Privilege(USERS_MANAGE_USERS);
-        Privilege p8 = new Privilege(USERS_PROHIBIT_VDC_RESTRICTION);
-        Privilege p11 = new Privilege(VDC_ENUMERATE);
+        Privilege p3 = new Privilege(Privileges.USERS_VIEW);
+        Privilege p4 = new Privilege(Privileges.USERS_VIEW_PRIVILEGES);
+        Privilege p5 = new Privilege(Privileges.USERS_MANAGE_USERS);
+        Privilege p8 = new Privilege(Privileges.USERS_PROHIBIT_VDC_RESTRICTION);
+        Privilege p11 = new Privilege(Privileges.VDC_ENUMERATE);
         return createInstance(p3, p4, p5, p8, p11);
     }
 
