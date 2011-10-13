79b8aadbb1ea98b30248275648dab4675b269a67
danielestevez <daniel.estevez@abiquo.com>
2011-10-13 13:39:11 +0200

    Merged from ABICLOUDPREMIUM-2504

--- database/kinton-schema.sql
+++ database/kinton-schema.sql
@@ -2011,7 +2011,6 @@ DROP TRIGGER IF EXISTS `kinton`.`update_network_configuration_update_stats`;
 DROP TRIGGER IF EXISTS `kinton`.`dclimit_created`;
 DROP TRIGGER IF EXISTS `kinton`.`dclimit_updated`;
 DROP TRIGGER IF EXISTS `kinton`.`dclimit_deleted`;
-DROP TRIGGER IF EXISTS `kinton`.`update_datastore_update_stats`;
 --
 -- Init Stats
 DELIMITER |
@@ -2367,7 +2366,7 @@ CREATE TRIGGER `kinton`.`update_datastore_update_stats` AFTER UPDATE ON `kinton`
 FOR EACH ROW BEGIN
 DECLARE idDatacenter INT UNSIGNED;
 DECLARE machineState INT UNSIGNED;
-SELECT pm.idDatacenter, pm.idState INTO idDatacenter, machineState FROM physicalmachine pm LEFT OUTER JOIN datastore_assignment da ON da.idPhysicalMachine = da.idPhysicalMachine
+SELECT pm.idDatacenter, pm.idState INTO idDatacenter, machineState FROM physicalmachine pm LEFT OUTER JOIN datastore_assignment da ON pm.idPhysicalMachine = da.idPhysicalMachine
 WHERE da.idDatastore = NEW.idDatastore;
 IF (@DISABLE_STATS_TRIGGERS IS NULL) THEN
     IF OLD.enabled = 1 THEN
@@ -2400,7 +2399,7 @@ IF (@DISABLE_STATS_TRIGGERS IS NULL) THEN
         END IF;
     END IF;
 END IF;
-END
+END;
 --
 --
 -- ******************************************************************************************
@@ -3479,45 +3478,6 @@ CREATE TRIGGER `kinton`.`dclimit_deleted` AFTER DELETE ON `kinton`.`enterprise_l
             publicIPsReserved = publicIPsReserved - OLD.publicIPHard,
             vlanReserved = vlanReserved - OLD.vlanHard
         WHERE idDataCenter = OLD.idDataCenter;
-    END;
-|
-CREATE TRIGGER `kinton`.`update_datastore_update_stats` AFTER UPDATE ON `kinton`.`datastore`
-    FOR EACH ROW BEGIN
-	DECLARE idDatacenter INT UNSIGNED;
-	DECLARE machineState INT UNSIGNED;
-	SELECT pm.idDatacenter, pm.idState INTO idDatacenter, machineState FROM physicalmachine pm LEFT OUTER JOIN datastore_assignment da ON pm.idPhysicalMachine = da.idPhysicalMachine
-	WHERE da.idDatastore = NEW.idDatastore;
-	IF (@DISABLE_STATS_TRIGGERS IS NULL) THEN
-	    IF OLD.enabled = 1 THEN
-		IF NEW.enabled = 1 THEN
-		    IF machineState IN (2, 6, 7) THEN
-		        UPDATE IGNORE cloud_usage_stats cus SET cus.vStorageTotal = cus.vStorageTotal - OLD.size + NEW.size
-		        WHERE cus.idDatacenter = idDatacenter;
-		    ELSE
-		        UPDATE IGNORE cloud_usage_stats cus SET cus.vStorageTotal = cus.vStorageTotal - OLD.size + NEW.size,
-		        cus.vStorageUsed = cus.vStorageUsed - OLD.usedSize + NEW.usedSize WHERE cus.idDatacenter = idDatacenter;
-		    END IF;
-		ELSEIF NEW.enabled = 0 THEN
-		    IF machineState IN (2, 6, 7) THEN
-		        UPDATE IGNORE cloud_usage_stats cus SET cus.vStorageTotal = cus.vStorageTotal - OLD.size
-		        WHERE cus.idDatacenter = idDatacenter;
-		    ELSE
-		        UPDATE IGNORE cloud_usage_stats cus SET cus.vStorageTotal = cus.vStorageTotal - OLD.size,
-		        cus.vStorageUsed = cus.vStorageUsed - OLD.usedSize WHERE cus.idDatacenter = idDatacenter;
-		    END IF;
-		END IF;
-	    ELSE
-		IF NEW.enabled = 1 THEN
-		    IF machineState IN (2, 6, 7) THEN
-		        UPDATE IGNORE cloud_usage_stats cus SET cus.vStorageTotal = cus.vStorageTotal + NEW.size
-		        WHERE cus.idDatacenter = idDatacenter;
-		    ELSE
-		        UPDATE IGNORE cloud_usage_stats cus SET cus.vStorageTotal = cus.vStorageTotal + NEW.size,
-		        cus.vStorageUsed = cus.vStorageUsed + NEW.usedSize WHERE cus.idDatacenter = idDatacenter;
-		    END IF;
-		END IF;
-	    END IF;
-	END IF;
     END;
 |
 -- ******************************************************************************************
