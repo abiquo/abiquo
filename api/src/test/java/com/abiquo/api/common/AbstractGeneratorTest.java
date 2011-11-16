8d87d65231d12b12eef055c63137f356e9df1b40
Sergi Castro <sergi.castro@abiquo.com>
2011-11-16 17:08:36 +0100

    modifying generators for stateful tests

--- api/src/test/java/com/abiquo/api/common/AbstractGeneratorTest.java
+++ api/src/test/java/com/abiquo/api/common/AbstractGeneratorTest.java
@@ -193,7 +193,7 @@ public abstract class AbstractGeneratorTest extends AbstractTestNGSpringContextT
             {"ip_pool_management", "volume_management", "diskstateful_conversions",
             "initiator_mapping", "rasd_management", "rasd", "nodevirtualimage", "nodenetwork",
             "nodestorage", "noderelationtype", "node", "virtualmachine", "virtualimage",
-            "category", "virtualimage_conversions", "node_virtual_image_stateful_conversions",
+            "virtualimage_conversions", "node_virtual_image_stateful_conversions",
             "virtual_appliance_conversions", "virtualapp", "vappstateful_conversions",
             "virtualdatacenter", "vlan_network", "vlan_network_assignment",
             "network_configuration", "dhcp_service", "storage_pool", "tier", "storage_device",
@@ -202,10 +202,11 @@ public abstract class AbstractGeneratorTest extends AbstractTestNGSpringContextT
             "repository", "workload_fit_policy_rule", "network", "session", "user",
             "roles_privileges", "role_ldap", "role", "privilege", "enterprise",
             "enterprise_limits_by_datacenter", "workload_enterprise_exclusion_rule",
-            "ovf_package_list_has_ovf_package", "ovf_package", "ovf_package_list", "apps_library",
-            "license", "system_properties", "vdc_enterprise_stats", "vapp_enterprise_stats",
-            "dc_enterprise_stats", "enterprise_resources_stats", "cloud_usage_stats", "log",
-            "metering", "tasks", "alerts", "heartbeatlog", "icon", "repository", "register"};
+            "ovf_package_list_has_ovf_package", "ovf_package", "ovf_package_list", "category",
+            "apps_library", "license", "system_properties", "vdc_enterprise_stats",
+            "vapp_enterprise_stats", "dc_enterprise_stats", "enterprise_resources_stats",
+            "cloud_usage_stats", "log", "metering", "tasks", "alerts", "heartbeatlog", "icon",
+            "repository", "register"};
 
         tearDown(entities);
     }
