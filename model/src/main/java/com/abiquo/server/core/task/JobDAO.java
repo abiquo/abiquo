2ffb13524c1e84dada27f4da1bf697a5c8bc9eaa
enricruiz <morelies@gmail.com>
2011-11-18 14:47:39 +0100

    License added

--- model/src/main/java/com/abiquo/server/core/task/JobDAO.java
+++ model/src/main/java/com/abiquo/server/core/task/JobDAO.java
@@ -1,3 +1,24 @@
+/**
+ * Abiquo community edition
+ * cloud management application for hybrid clouds
+ * Copyright (C) 2008-2010 - Abiquo Holdings S.L.
+ *
+ * This application is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC
+ * LICENSE as published by the Free Software Foundation under
+ * version 3 of the License
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * LESSER GENERAL PUBLIC LICENSE v.3 for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 package com.abiquo.server.core.task;
 
 import static com.abiquo.model.redis.RedisEntityUtils.getEntityKey;
@@ -6,6 +27,8 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.stereotype.Component;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Transaction;
@@ -16,6 +39,7 @@ import com.abiquo.model.redis.RedisEntityUtils;
 import com.abiquo.server.core.task.Job.JobState;
 import com.abiquo.server.core.task.Job.JobType;
 
+@Component
 public class JobDAO extends RedisDAOBase<Job>
 {
     protected final KeyMaker keyMaker = new KeyMaker(Job.class);
