ca0ac76347686f7dc8bc58bd9d2d01658e88d3a9
Alessia Prete <alessia.prete@abiquo.com>
2011-11-24 11:44:05 +0100

    Changed message APIerror

--- api/src/main/java/com/abiquo/api/exceptions/APIError.java
+++ api/src/main/java/com/abiquo/api/exceptions/APIError.java
@@ -365,7 +365,7 @@ public enum APIError
 
     // QUERY PAGGING STANDARD ERRORS
     QUERY_INVALID_PARAMETER("QUERY-0", "Invalid 'by' parameter"), QUERY_NETWORK_TYPE_INVALID_PARAMETER(
-        "QUERY-1", "Invalid 'type' parameter. Only 'EXTERNAL' or 'PUBLIC' allowed"),
+        "QUERY-1", "Invalid 'type' parameter. Only 'EXTERNAL', 'UNMANAGED' or 'PUBLIC' allowed"),
 
     VOLUME_GENERIC_ERROR("VOL-0", "Could not create the volume in the selected tier"), VOLUME_NOT_ENOUGH_RESOURCES(
         "VOL-1", "There are not enough resources in the selected tier to create the volume"), VOLUME_NAME_NOT_FOUND(
