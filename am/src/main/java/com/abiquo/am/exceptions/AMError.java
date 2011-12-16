e0519a67237bc1ad8128a93d1e90be848501d2e6
Albert Puig Mercadé <apuig@abiquo.com>
2011-12-16 11:02:45 +0100

    MJ improves the am errors

--- am/src/main/java/com/abiquo/am/exceptions/AMError.java
+++ am/src/main/java/com/abiquo/am/exceptions/AMError.java
@@ -28,45 +28,50 @@ public enum AMError
 {
 
     AM_CHECK("AM-CHECK",
-        "Appliance Manager is not well configured. Check the repository filesystem"), //
-    AM_CHECK_REDIS("AM-CHECK-REDIS", "No connection to the redis server"), //
-    AM_NOTIFICATION("AM-NOTIFICATION", "Can not publish an outgoing status message"), //
-    TEMPLATE_INVALID("TEMPLATE-INVALID", "Invalid OVF document"), //
-    TEMPLATE_INVALID_LOCATION("TEMPLATE-INVALID-LOC", "Invalid OVF location"), //
+        "Appliance Manager is not properly configured. Check the repository file system"), //
+    AM_CHECK_REDIS("AM-CHECK-REDIS", "No connection to the Redis Server"), //
+    AM_NOTIFICATION("AM-NOTIFICATION",
+        "Cannot update Template status because cannot publish download status message in RabbitMQ"), //
+    TEMPLATE_INVALID("TEMPLATE-INVALID", "Invalid OVF Document"), //
+    TEMPLATE_INVALID_LOCATION("TEMPLATE-INVALID-LOC", "Invalid OVF URL"), //
     TEMPLATE_INVALID_MULTIPLE_FILES("TEMPLATE-INVALID-MULTIPLE-FILE",
-        "OVF document contains more than a single Referenced File, one none."), TEMPLATE_NOT_FOUND(
-        "TEMPLATE-NOT-FOUND", "OVF document not found in the repository"), //
-    TEMPLATE_MALFORMED("TEMPLATE-MALFORMED", "OVF document can not be read"), //
-    TEMPLATE_INSTALL("TEMPLATE-INSTALL", "Can't create the ovf package folder"), //
-    TEMPLATE_INSTALL_ALREADY("TEMPLATE-INSTALL-ALREADY", "OVF already exist"), //
-    TEMPLATE_DOWNLOAD("TEMPLATE-DOWNLOAD", "Can't download the ovf package"), //
-    TEMPLATE_CANCEL("TEMPLATE-CANCEL", "Can't cancel the ovf package install"), //
-    REPO_NO_SPACE("REPO-NO-SPACE", "No space left in the repository file system"), //
-    TEMPLATE_BOUNDLE("TEMPLATE-BOUNDLE", "Can't create the instance"), //
-    TEMPLATE_UPLOAD("TEMPLATE-UPLOAD", "Can't upload the ovf package"), //
+        "OVF document contains more than one Referenced File or none."), TEMPLATE_NOT_FOUND(
+        "TEMPLATE-NOT-FOUND", "OVF Document not found in the Template Repository"), //
+    TEMPLATE_MALFORMED("TEMPLATE-MALFORMED", "OVF Document cannot be read"), //
+    TEMPLATE_INSTALL("TEMPLATE-INSTALL",
+        "Cannot create the Template folder on the NFS Repository file system"), //
+    TEMPLATE_INSTALL_ALREADY("TEMPLATE-INSTALL-ALREADY", "Template already exists"), //
+    TEMPLATE_DOWNLOAD("TEMPLATE-DOWNLOAD", "Cannot download the Template"), //
+    TEMPLATE_CANCEL("TEMPLATE-CANCEL", "Cannot cancel the Template download"), //
+    REPO_NO_SPACE("REPO-NO-SPACE", "No space left on the NFS Repository file system"), //
+    TEMPLATE_BOUNDLE("TEMPLATE-BOUNDLE", "Cannot create the Template Instance"), //
+    TEMPLATE_UPLOAD("TEMPLATE-UPLOAD", "Cannot upload the Template"), //
     REPO_NOT_ACCESSIBLE("REPO-NOT-ACCESSIBLE",
-        "Repository is not accessible. Check the exported location (propably NFS is stopped), "
-            + "or havent write permision"), //
+        "Repository is not accessible. Check the exported location (NFS is probably down "
+            + "or no write permission"), //
     REPO_TIMEOUT_REFRESH("REPO-TIMEOUT-REFRESH",
-        "Timeout during repository file system synchronization."), //
-    TEMPLATE_DELETE("OVFPI-DELETE", "Can't delete ovf package content"), //
-    TEMPLATE_DELETE_INSTANCES("OVFPI-DELETE", "Can't delete ovf package content. It have instances"), //
+        "Timeout during NFS Repository file system refresh."), //
+    TEMPLATE_DELETE("OVFPI-DELETE", "Cannot delete Template folder"), //
+    TEMPLATE_DELETE_INSTANCES("OVFPI-DELETE",
+        "Cannot delete Template content because there are instances in the folder"), //
     TEMPLATE_SNAPSHOT_ALREADY_EXIST("TEMPLATE-SNAPSHOT-ALREADY-EXIST",
-        "Can't create snapshot, other with the same name exist"), //
+        "Cannot create Template Instance because another one with the same name already exists"), //
     TEMPLATE_SNAPSHOT_CREATE("TEMPLATE-SNAPSHOT-CREATE",
-        "Can't create snapshot, invalid ovf xml document"), //
-    TEMPLATE_UNKNOW_STATUS("TEMPLATE-UNKNOW-STATUS", "Can't obtain the ovf package instance status"), //
-    TEMPLATE_CHANGE_STATUS(
-        "TEMPLATE-CHANGE-STATUS",
-        "Can't update the ovf package instance status. Can't create the file mark in the repository file system"), //
-    DISK_FILE_MOVE("DISK-FILE-MOVE", "Disk failed to move into the repository"), //
-    TEMPLATE_SNAPSHOT_IMPORT_NOT_EXIST(
-        "TEMPLATE-SNAPSHOT-IMPORT-NOT-EXIST",
-        "Imported snapshot not found. Should be a bundle of an imported virtual machine or isn't a directory"), //
-    DISK_FILE_NOT_FOUND("DISK-NOT-FOUND", "Requested disk doesn't exist in the current repository"), //
-    DISK_FILE_COPY_ERROR("DISK-FILE-COPY", "Some error during disk file copy"), //
-    DISK_FILE_ALREADY_EXIST("DISK-ALREADY-EXIST",
-        "The destination path of the copy already exist in the current repository");
+        "Cannot create Template Instance because the parent OVF Document is invalid"), //
+    TEMPLATE_UNKNOW_STATUS("TEMPLATE-UNKNOW-STATUS", "Cannot obtain the Template status"), //
+    TEMPLATE_CHANGE_STATUS("TEMPLATE-CHANGE-STATUS",
+        "Cannot update Template status because cannot create file marker on NFS Repository file system"), //
+    DISK_FILE_MOVE("DISK-FILE-MOVE",
+        "Failed to move disk from the XenServer folder to the NFS Repository for Instance"), //
+    TEMPLATE_SNAPSHOT_IMPORT_NOT_EXIST("TEMPLATE-SNAPSHOT-IMPORT-NOT-EXIST",
+        "Imported Template Instance not found. Should be an Instance of an imported virtual machine."), //
+    DISK_FILE_NOT_FOUND("DISK-NOT-FOUND",
+        "Disk requested for XenServer deployment does not exist in the current NFS Repository"), //
+    DISK_FILE_COPY_ERROR("DISK-FILE-COPY",
+        "An error occurred during copy of disk from NFS Repository to XenServer folder for deployment"), //
+    DISK_FILE_ALREADY_EXIST(
+        "DISK-ALREADY-EXIST",
+        "The destination path of the copy already exists in the XenServer folder on the NFS Repository file system");
 
     /**
      * Internal error code
