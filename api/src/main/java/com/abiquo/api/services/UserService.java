8c56f95b5f37af8cdb8dcdb67dd87985b3a2735f
zmalik <zain.malik@abiquo.com>
2011-12-16 11:26:12 +0100

    [ABICLOUDPREMIUM-2726] User model updated with same values as in db

--- api/src/main/java/com/abiquo/api/services/UserService.java
+++ api/src/main/java/com/abiquo/api/services/UserService.java
@@ -223,8 +223,7 @@ public class UserService extends DefaultApiService
                 .getNick(), encrypt(dto.getPassword()), dto.getLocale());
         user.setActive(dto.isActive() ? 1 : 0);
         user.setDescription(dto.getDescription());
-        user.isValid();
-
+        validate(user);
         if (securityService.hasPrivilege(Privileges.USERS_PROHIBIT_VDC_RESTRICTION, user))
         {
             user.setAvailableVirtualDatacenters(null);
@@ -473,7 +472,13 @@ public class UserService extends DefaultApiService
 
     private Role findRole(final UserDto dto)
     {
-        return repo.findRoleById(getRoleId(dto));
+        Role role = repo.findRoleById(getRoleId(dto));
+        if (role == null)
+        {
+            addNotFoundErrors(APIError.NON_EXISTENT_ROLE);
+            flushErrors();
+        }
+        return role;
     }
 
     private Integer getRoleId(final UserDto user)
