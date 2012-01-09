fded1e22f85737d3ea138f07ad568bb6ea9d68ea
Susana Acedo <susana.acedo@abiquo.com>
2012-01-09 11:37:34 +0100

    dhcp options

--- api/src/main/java/com/abiquo/api/services/stub/TarantinoJobCreator.java
+++ api/src/main/java/com/abiquo/api/services/stub/TarantinoJobCreator.java
@@ -15,6 +15,27 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/**
+ * Abiquo community edition
+ * cloud management application for hybrid clouds
+ * Copyright (C) 2008-2010 - Abiquo Holdings S.L.
+ *
+ * This application is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC
+ * LICENSE as published by the Free Software Foundation under
+ * version 3 of the License
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * LESSER GENERAL PUBLIC LICENSE v.3 for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
  * Boston, MA 02111-1307, USA.
  */
 
@@ -447,13 +468,14 @@ public class TarantinoJobCreator extends DefaultApiService
 
         for (DhcpOption d : dhcplist)
         {
-            DhcpOptionCom dchp =
-                new DhcpOptionCom(d.getOption(),
-                    d.getGateway(),
-                    d.getNetworkAddress(),
-                    d.getMask(),
-                    d.getNetmask());
-            dhcpComList.add(dchp);
+            DhcpOptionCom dhcp = new DhcpOptionCom();
+            dhcp.setMask(d.getMask());
+            dhcp.setOption(d.getOption());
+            dhcp.setNetworkAddress(d.getNetworkAddress());
+            dhcp.setGateway(d.getGateway());
+            dhcp.setNetmask(d.getNetmask());
+
+            dhcpComList.add(dhcp);
         }
         return dhcpComList;
 
