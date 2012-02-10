309681327b6c39be086f799d42346f771c13ee86
Albert Puig Mercadé <apuig@abiquo.com>
2012-02-10 15:54:29 +0100

    copyVirtualDisks modifies the snapshot process

--- model/src/main/java/com/abiquo/model/enumerator/HypervisorType.java
+++ model/src/main/java/com/abiquo/model/enumerator/HypervisorType.java
@@ -36,8 +36,8 @@ import org.apache.commons.lang.ArrayUtils;
 public enum HypervisorType
 {
     VBOX(8889, VDI_FLAT, VBOX_COMPATIBLES), KVM(8889, VMDK_FLAT, KVM_COMPATIBLES), XEN_3(8889,
-        VMDK_FLAT, XEN_COMPATIBLES), VMX_04(443, VMDK_FLAT, VMWARE_COMPATIBLES), HYPERV_301(5985,
-        VHD_SPARSE, HYPERV_COMPATIBLES), XENSERVER(9363, VHD_SPARSE, XENSERVER_COMPATIBLES);
+        VMDK_FLAT, XEN_COMPATIBLES), VMX_04(443, VMDK_FLAT, VMWARE_COMPATIBLES, VMDK_FLAT), HYPERV_301(
+        5985, VHD_SPARSE, HYPERV_COMPATIBLES), XENSERVER(9363, VHD_SPARSE, XENSERVER_COMPATIBLES);
 
     public final int defaultPort;
 
@@ -45,7 +45,18 @@ public enum HypervisorType
 
     public DiskFormatType[] compatibilityTable;
 
+    public DiskFormatType instanceFormat;
+
     /* package */private final static int ID_MAX = 6;
+
+    private HypervisorType(final int defaultPort, final DiskFormatType baseFormat,
+        final DiskFormatType[] compatibilityTable, final DiskFormatType instanceFormat)
+    {
+        this.defaultPort = defaultPort;
+        this.baseFormat = baseFormat;
+        this.compatibilityTable = compatibilityTable;
+        this.instanceFormat = instanceFormat;
+    }
 
     private HypervisorType(final int defaultPort, final DiskFormatType baseFormat,
         final DiskFormatType[] compatibilityTable)
@@ -58,6 +69,16 @@ public enum HypervisorType
     public int id()
     {
         return ordinal() + 1;
+    }
+
+    public boolean isInstanceFormatFixed()
+    {
+        return instanceFormat != null;
+    }
+
+    public DiskFormatType getInstanceFormat()
+    {
+        return instanceFormat;
     }
 
     public boolean isCompatible(final DiskFormatType type)
