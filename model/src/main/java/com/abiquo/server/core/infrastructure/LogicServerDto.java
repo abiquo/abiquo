2d08254506b4db60f48ef43d2ad3e65184d5eef7
Francesc Montserrat <fmontserrat.carvajal@gmail.com>
2012-02-23 16:19:18 +0100

    UCS single resource transport

--- model/src/main/java/com/abiquo/server/core/infrastructure/LogicServerDto.java
+++ model/src/main/java/com/abiquo/server/core/infrastructure/LogicServerDto.java
@@ -36,10 +36,12 @@ import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlElements;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.abiquo.model.transport.SingleResourceTransportDto;
+
 /**
  */
 @XmlRootElement(name = "logicServer")
-public class LogicServerDto
+public class LogicServerDto extends SingleResourceTransportDto
 {
 
     protected String name;
