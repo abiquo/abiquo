6addb85f7c7e51d1040d67165e1fe4464f90211f
Serafin Sedano <serafin.sedano@abiquo.com>
2012-03-02 14:52:50 +0100

    Versioning WIP

--- model/src/main/java/com/abiquo/server/core/cloud/chef/BootstrapDto.java
+++ model/src/main/java/com/abiquo/server/core/cloud/chef/BootstrapDto.java
@@ -29,13 +29,16 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
+import com.abiquo.model.transport.SingleResourceTransportDto;
+
 @XmlRootElement(name = "bootstrap")
 @XmlType(propOrder = {"nodeName", "chefConfig"})
 public class BootstrapDto implements Serializable
 {
     public static final String BASE_MEDIA_TYPE = "application/vnd.abiquo.extended-bootstrap+xml";
 
-    public static final String MEDIA_TYPE = BASE_MEDIA_TYPE + "; version=" + "1.0";
+    public static final String MEDIA_TYPE = BASE_MEDIA_TYPE + "; version="
+        + SingleResourceTransportDto.API_VERSION;
 
     private static final long serialVersionUID = 1L;
 
