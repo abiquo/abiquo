f3dc9d17240b0a5fff8e13474b11539b2bfc7d45
Mary-Jane Smyth <maryjane.smyth@gmail.com>
2012-03-08 19:45:34 +0100

    @apuig @xfernandez [ABICLOUDPREMIUM-3410] Revise English and meaning of Appliance Manager Error Messages. FIXED

--- am/src/main/java/com/abiquo/am/exceptions/AMError.java
+++ am/src/main/java/com/abiquo/am/exceptions/AMError.java
@@ -35,42 +35,41 @@ public enum AMError
     TEMPLATE_INVALID("TEMPLATE-INVALID", "Invalid OVF Document"), //
     TEMPLATE_INVALID_LOCATION("TEMPLATE-INVALID-LOC", "Invalid OVF URL"), //
     TEMPLATE_INVALID_MULTIPLE_DISKS("TEMPLATE-INVALID-MULTIPLE-DISK",
-        "OVF document contains more than one Referenced Disks or none."), TEMPLATE_INVALID_MULTIPLE_FILES(
+        "OVF document contains no referenced disk or more than one."), TEMPLATE_INVALID_MULTIPLE_FILES(
         "TEMPLATE-INVALID-MULTIPLE-FILE",
-        "OVF document contains more than one Referenced Files or none."), TEMPLATE_INVALID_DISK_REFERENCE(
+        "OVF document contains no referenced files or more than one."), TEMPLATE_INVALID_DISK_REFERENCE(
         "TEMPLATE-INVALID-DISK-REFRENCE",
-        "In Virtual Hardware Section no reference to image disk is found."), TEMPLATE_NOT_FOUND(
+        "Virtual Hardware Section contains no reference to the disk."), TEMPLATE_NOT_FOUND(
         "TEMPLATE-NOT-FOUND", "OVF Document not found in the Template Repository"), //
     TEMPLATE_MALFORMED("TEMPLATE-MALFORMED", "OVF Document cannot be read"), //
     TEMPLATE_INSTALL("TEMPLATE-INSTALL",
-        "Cannot create the Template folder on the NFS Repository file system"), //
+        "Cannot create the template folder on the NFS Repository file system"), //
     TEMPLATE_INSTALL_ALREADY("TEMPLATE-INSTALL-ALREADY", "Template already exists"), //
-    TEMPLATE_DOWNLOAD("TEMPLATE-DOWNLOAD", "Cannot download the Template"), //
-    TEMPLATE_CANCEL("TEMPLATE-CANCEL", "Cannot cancel the Template download"), //
+    TEMPLATE_DOWNLOAD("TEMPLATE-DOWNLOAD", "Cannot download the template"), //
+    TEMPLATE_CANCEL("TEMPLATE-CANCEL", "Cannot cancel the template download"), //
     REPO_NO_SPACE("REPO-NO-SPACE", "No space left on the NFS Repository file system"), //
-    TEMPLATE_BOUNDLE("TEMPLATE-BOUNDLE", "Cannot create the Template Instance"), //
-    TEMPLATE_UPLOAD("TEMPLATE-UPLOAD", "Cannot upload the Template"), //
+    TEMPLATE_BOUNDLE("TEMPLATE-BOUNDLE", "Cannot create the template instance"), //
+    TEMPLATE_UPLOAD("TEMPLATE-UPLOAD", "Cannot upload the template"), //
     REPO_NOT_ACCESSIBLE(
         "REPO-NOT-ACCESSIBLE",
-        "Repository is not accessible. "
-            + "Check the repositoryLocation property and that the NFS Server is up and Abiquo has write permission."), //
+        "NFS Repository may not be available or accessible to Abiquo Remote Services. Check the repositoryLocation in abiquo.properties."), //
 
     REPO_TIMEOUT_REFRESH("REPO-TIMEOUT-REFRESH",
         "Timeout during NFS Repository file system refresh."), //
-    TEMPLATE_DELETE("OVFPI-DELETE", "Cannot delete Template folder"), //
+    TEMPLATE_DELETE("OVFPI-DELETE", "Cannot delete template folder"), //
     TEMPLATE_DELETE_INSTANCES("OVFPI-DELETE",
-        "Cannot delete Template content because there are instances in the folder"), //
+        "Cannot delete template content because there are instances in the folder"), //
     TEMPLATE_SNAPSHOT_ALREADY_EXIST("TEMPLATE-SNAPSHOT-ALREADY-EXIST",
-        "Cannot create Template Instance because another one with the same name already exists"), //
+        "Cannot create template instance because another one with the same name already exists"), //
     TEMPLATE_SNAPSHOT_CREATE("TEMPLATE-SNAPSHOT-CREATE",
-        "Cannot create Template Instance because the parent OVF Document is invalid"), //
-    TEMPLATE_UNKNOW_STATUS("TEMPLATE-UNKNOW-STATUS", "Cannot obtain the Template status"), //
+        "Cannot create template instance because the master OVF Document is invalid"), //
+    TEMPLATE_UNKNOW_STATUS("TEMPLATE-UNKNOW-STATUS", "Cannot obtain the template status"), //
     TEMPLATE_CHANGE_STATUS("TEMPLATE-CHANGE-STATUS",
-        "Cannot update Template status because cannot create file marker on NFS Repository file system"), //
+        "Cannot update template status because cannot create file marker on NFS Repository file system"), //
     DISK_FILE_MOVE("DISK-FILE-MOVE",
-        "Failed to move disk from the XenServer folder to the NFS Repository for Instance"), //
+        "Failed to move disk from the XenServer folder to the NFS Repository for instance"), //
     TEMPLATE_SNAPSHOT_IMPORT_NOT_EXIST("TEMPLATE-SNAPSHOT-IMPORT-NOT-EXIST",
-        "Imported Template Instance not found. Should be an Instance of an imported virtual machine."), //
+        "Imported template instance not found. Should be an instance of an imported virtual machine."), //
     DISK_FILE_NOT_FOUND("DISK-NOT-FOUND",
         "Disk requested for XenServer deployment does not exist in the current NFS Repository"), //
     DISK_FILE_COPY_ERROR("DISK-FILE-COPY",
@@ -79,17 +78,17 @@ public enum AMError
         "DISK-ALREADY-EXIST",
         "The destination path of the copy already exists in the XenServer folder on the NFS Repository file system"), //
 
-    MOUNT_FILE_NOT_FOUND("MOUNT-0", "Can't find ''/etc/mtab'' to find mounted repositories."), //
-    MOUNT_FILE_READ_ERROR("MOUNT-1", "Can't read ''/etc/mtab'' to find mounted repositories."), //
+    MOUNT_FILE_NOT_FOUND("MOUNT-0", "Cannot find ''/etc/mtab'' to check mounted repositories."), //
+    MOUNT_FILE_READ_ERROR("MOUNT-1", "Cannot read ''/etc/mtab'' to check mounted repositories."), //
     MOUNT_INVALID_REPOSITORY("MOUNT-2",
         "The ''abiquo.appliancemanager.repositoryLocation'' (NFS export location) "
-            + "isn't mounted in ''abiquo.appliancemanager.localRepositoryPath''"), //
+            + "is not mounted at ''abiquo.appliancemanager.localRepositoryPath''"), //
     CONFIG_REPOSITORY_LOCATION("CONF-0",
-        "Invalid ''abiquo.appliancemanager.repositoryLocation'' configuration elemente"), //
+        "Invalid ''abiquo.appliancemanager.repositoryLocation'' configuration element"), //
     CONFIG_REPOSITORY_PATH("CONF-1",
-        "Invalid ''abiquo.appliancemanager.localRepositoryPath'' configuration elemente"), //
-    CONFIG_REPOSITORY_MARK("CONF-2", "Repository is mounted on the expected mount point, "
-        + "but the ''.abiquo_repository'' is not found and can't be created")//
+        "Invalid ''abiquo.appliancemanager.localRepositoryPath'' configuration element"), //
+    CONFIG_REPOSITORY_MARK("CONF-2", "Repository is mounted with the expected mount point, "
+        + "but the ''.abiquo_repository'' file marker is not found and cannot be created")//
     ;
 
     /**
