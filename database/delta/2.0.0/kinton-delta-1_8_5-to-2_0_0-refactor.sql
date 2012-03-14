333fa20f5ceff8cf94b3d57eedcfdcfea97c8382
Xavier Thevenot <xthevenot@abiquo.com>
2012-03-14 15:32:37 +0100

    [ABICLOUDPREMIUM-3328]enterprise admins can change the settings of the shared images.FIXED

--- database/delta/2.0.0/kinton-delta-1_8_5-to-2_0_0-refactor.sql
+++ database/delta/2.0.0/kinton-delta-1_8_5-to-2_0_0-refactor.sql
@@ -695,21 +695,6 @@ BEGIN
 	IF @existsCount = 0 THEN 
 		INSERT INTO kinton.roles_privileges VALUES (1,50,0);
 	END IF;
-	
-	-- CHEF --
-	-- Dumping data for table kinton.privilege
-	SELECT COUNT(*) INTO @existsCount FROM kinton.privilege WHERE idPrivilege='51' AND name='USERS_MANAGE_CHEF_ENTERPRISE';
-	IF @existsCount = 0 THEN 
-		INSERT INTO kinton.privilege VALUES (51,'USERS_MANAGE_CHEF_ENTERPRISE',0);
-	END IF;
-
-	-- CHEF --
-	-- Dumping data for table kinton.roles_privileges
-	--
-	SELECT COUNT(*) INTO @existsCount FROM kinton.roles_privileges WHERE idRole='1' AND idPrivilege='51';
-	IF @existsCount = 0 THEN 
-		INSERT INTO kinton.roles_privileges VALUES (1,51,0);
-	END IF;
 
 	-- CHECK: Is this needed?
 	-- First I need to update some rows before to delete the default_network field
