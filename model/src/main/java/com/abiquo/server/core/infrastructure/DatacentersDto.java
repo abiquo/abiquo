61785a4126c7aebaff943d8726d3309e34c35aef
Susana Acedo <susana.acedo@abiquo.com>
2012-03-29 14:54:25 +0200

    ABICLOUDPREMIUM-3546, datacenter with resources

--- model/src/main/java/com/abiquo/server/core/infrastructure/DatacentersDto.java
+++ model/src/main/java/com/abiquo/server/core/infrastructure/DatacentersDto.java
@@ -35,20 +35,24 @@ import com.abiquo.model.transport.WrapperDto;
 public class DatacentersDto extends WrapperDto<DatacenterDto>
 {
     public static final String BASE_MEDIA_TYPE = "application/vnd.abiquo.datacenters+xml";
+
+    public static final String RS_MEDIA_TYPE = "application/vnd.abiquo.datacenters_rs+xml";
+
     public static final String MEDIA_TYPE = BASE_MEDIA_TYPE + "; version=" + API_VERSION;
-    
+
+    @Override
     @XmlElement(name = "datacenter")
     public List<DatacenterDto> getCollection()
     {
         return collection;
     }
-    
+
     @Override
     public String getMediaType()
     {
         return DatacentersDto.MEDIA_TYPE;
     }
-    
+
     @Override
     public String getBaseMediaType()
     {
