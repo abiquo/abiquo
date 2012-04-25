71630f17e65c522ffea72dab2e4e8a59b26b9b18
danielestevez <daniel.estevez@abiquo.com>
2012-04-25 16:40:57 +0200

    Deltas samples for testing.

--- database/kinton_liquibase_generated.sql
+++ database/kinton_liquibase_generated.sql
@@ -1,10 +1,9 @@
-Liquibase Home: /home/destevezg/dev/liquibase/
 -- *********************************************************************
 -- Update Database Script
 -- *********************************************************************
 -- Change Log: /home/destevezg/abiws/abiquo/database/changelog.xml
--- Ran at: 4/25/12 4:05 PM
--- Against: root@localhost@jdbc:mysql://localhost:3306/kinton_liquibase
+-- Ran at: 4/24/12 1:03 PM
+-- Against: root@destevez.bcn.abiquo.com@jdbc:mysql://10.60.12.230:3306/kinton_liquibase
 -- Liquibase version: 2.0.3
 -- *********************************************************************
 
@@ -2851,23 +2850,4 @@ INSERT INTO `kinton`.`DATABASECHANGELOG` (`AUTHOR`, `COMMENTS`, `DATEEXECUTED`, 
 CREATE UNIQUE INDEX `user_auth_idx` ON `kinton`.`user`(`user`, `authType`);
 
 INSERT INTO `kinton`.`DATABASECHANGELOG` (`AUTHOR`, `COMMENTS`, `DATEEXECUTED`, `DESCRIPTION`, `EXECTYPE`, `FILENAME`, `ID`, `LIQUIBASE`, `MD5SUM`, `ORDEREXECUTED`) VALUES ('destevezg (generated)', '', NOW(), 'Create Index', 'EXECUTED', '/home/destevezg/abiws/abiquo/database/changelog.xml', '1334595170508-189', '2.0.3', '3:4eff3205127c7bc1a520db1b06261792', 567);
-
--- Changeset delta/deltachangelogHF1.xml::1335280201177-1::destevezg (generated)::(Checksum: 3:fd4f14896387e0fd842ac1cbc5b89239)
-CREATE TABLE `kinton`.`accounting_event_detail_HF1` (`idAccountingEvent` BIGINT AUTO_INCREMENT NOT NULL, `startTime` TIMESTAMP DEFAULT '0000-00-00 00:00:00' NOT NULL, `endTime` TIMESTAMP DEFAULT '0000-00-00 00:00:00' NOT NULL, `idAccountingResourceType` TINYINT NOT NULL, `resourceType` VARCHAR(255) NOT NULL, `resourceUnits` BIGINT NOT NULL, `resourceName` VARCHAR(511) NOT NULL, `idEnterprise` INT UNSIGNED NOT NULL, `idVirtualDataCenter` INT UNSIGNED NOT NULL, `idVirtualApp` INT UNSIGNED, `idVirtualMachine` INT UNSIGNED, `enterpriseName` VARCHAR(255) NOT NULL, `virtualDataCenter` VARCHAR(255) NOT NULL, `virtualApp` VARCHAR(255), `virtualMachine` VARCHAR(255), `costCode` INT, `idStorageTier` INT, `version_c` INT DEFAULT 0, CONSTRAINT `PK_ACCOUNTING_EVENT_DETAIL_HF1` PRIMARY KEY (`idAccountingEvent`));
-
-INSERT INTO `kinton`.`DATABASECHANGELOG` (`AUTHOR`, `COMMENTS`, `DATEEXECUTED`, `DESCRIPTION`, `EXECTYPE`, `FILENAME`, `ID`, `LIQUIBASE`, `MD5SUM`, `ORDEREXECUTED`) VALUES ('destevezg (generated)', '', NOW(), 'Create Table', 'EXECUTED', 'delta/deltachangelogHF1.xml', '1335280201177-1', '2.0.3', '3:fd4f14896387e0fd842ac1cbc5b89239', 568);
-
--- Changeset delta/deltachangelogHF1.xml::ABICLOUDPREMIUM-432432::destevez::(Checksum: 3:1fd743ccdf084124370692129205fcd8)
-DROP TRIGGER IF EXISTS kinton.datacenter_created;
-
-CREATE TRIGGER kinton.datacenter_created AFTER INSERT ON kinton.datacenter
-  FOR EACH ROW BEGIN
-    IF (@DISABLE_STATS_TRIGGERS IS NULL) THEN
-      INSERT IGNORE INTO cloud_usage_stats (idDataCenter) VALUES (NEW.idDataCenter);
-
-END IF;
-
-END;
-
-INSERT INTO `kinton`.`DATABASECHANGELOG` (`AUTHOR`, `COMMENTS`, `DATEEXECUTED`, `DESCRIPTION`, `EXECTYPE`, `FILENAME`, `ID`, `LIQUIBASE`, `MD5SUM`, `ORDEREXECUTED`) VALUES ('destevez', '', NOW(), 'SQL From File', 'EXECUTED', 'delta/deltachangelogHF1.xml', 'ABICLOUDPREMIUM-432432', '2.0.3', '3:1fd743ccdf084124370692129205fcd8', 569);
 
